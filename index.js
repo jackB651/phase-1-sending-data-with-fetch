49e85a601c9dbe46cc21216c861fdf45358c8566
Jonathon <jwbialas@gmail.com>
2022-07-06 22:11:07 -0500

    commit tree 6dd08e224d008e06410691568122def6fff77ed7

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const URL = "http://localhost:3000/users"
+
+function submitData (name, email){
+       return fetch (URL, {
+        method:"POST",
+        headers:{
+          'Content-Type':'application/json',
+          'Accept':'application/json'
+        }, 
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        }) 
+       } ).then(Response=>Response.json()).then(data=>{
+        const ID = data.id   
+        document.body.append(ID)
+       }).catch(message=>{
+        document.body.append(message)
+       })
+}
